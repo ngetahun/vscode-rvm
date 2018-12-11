37f58ae840344a2d87c40f3f747cdaebec12eaf6
Natnael Getahun <connect@ngetahun.me>
2018-12-11 07:14:29 +0100

    :recycle: Refactor types and interfaces

--- src/interpreter/configurator.ts
+++ src/interpreter/configurator.ts
@@ -1,91 +1,54 @@
 //import * as process from 'process';
-import * as os from 'os';
-import {spawn, exec} from 'child_process';
+import {IConfigurator, InterpreterManager, ConfiguratorType} from './types';
 
-interface IConfigurator {
-    findRVM(): Promise<boolean>;
-    findCurrent(): String;
-    listGems(): String[];
-    updateConfig(): boolean;
-    checkRVMVersion(): boolean;
-    //readonly MinimumRVMSupported: String; // for now, this will be left empty
-}
 /**
  * A configuration singleton to handle interpreter version
  * @author Natnael Getahun <connect@ngetahun.me>
  */
 export default class Configurator implements IConfigurator {
     private static mInstance: Configurator;
-    private rubyInterpreterVersion: String;
-    private isFirstTimeSettingExt = true;
+    // private rubyInterpreterVersion: String;
+
+    private isFirstTimeSettingExt = true; 
+    private configObject: ConfiguratorType;
 
     private constructor() {
         this.isFirstTimeSettingExt = true;
-        this.rubyInterpreterVersion = this.findCurrent();
-        this.isFirstTimeSettingExt = false;
+
+        // initialize it to use system installed ruby
+        this.configObject = {
+            configuredInterpreterManager: InterpreterManager.SYSTEM
+        };
     }
 
-    private getTargetMachine(): String {
-        let target = os.type();
-        if(['Darwin', 'Linux', 'Windows_NT'].indexOf(target) === -1) {
-            return 'Unknown target';
-        }
+    // private getTargetMachine(): String {
+    //     let target = os.type();
+    //     if(['Darwin', 'Linux', 'Windows_NT'].indexOf(target) === -1) {
+    //         return 'Unknown target';
+    //     }
 
-        return target;
-    }
-    async findRVM(): Promise<boolean> {
-        const targetMachine:String = this.getTargetMachine();
-        if(targetMachine.includes('Darwin')) { // OSX
-            let execd = await spawn('rvm', ['current'], {shell: true});
+    //     return target;
+    // }
+    // async findRVM(): Promise<boolean> {
+    //     const targetMachine:String = this.getTargetMachine();
+    //     if(targetMachine.includes('Darwin')) { // OSX
+    //         let execd = await spawn('rvm', ['current'], {shell: true});
 
-        }
-        else if (targetMachine.includes('Linux')) { // Linux
+    //     }
+    //     else if (targetMachine.includes('Linux')) { // Linux
 
-        }
-        else { // windows
+    //     }
+    //     else { // windows
 
-        }
-        return false;
-    }
+    //     }
+    //     return false;
+    // }
 
     /**
-     * Return the current ruby version
-     * @param
-     * @return String
+     * @returns boolean execution status
      */
-    findCurrent(): String {
-        let cmd = 'rvm current';
-        spawn(cmd)
-        return '';
-    }
-
-    /**
-     * List the gems for the current ruby 
-     * @param
-     * @return String[]
-     */
-    listGems(): String[] {
-        return new Array("");
-    }
-
-    /**
-     * Update hook to sync the Configurator
-     * return a boolean signifying success
-     * @param
-     * @return boolean
-     */
-    updateConfig(): boolean {
-        return false;
-    }
-
-    /**
-     * A check to see if the user has the desired 
-     * rvm version
-     * @param
-     * @return boolean
-     */
-    checkRVMVersion(): boolean {
-        return false;
+    async onConfigUpdate(): Promise<boolean> {
+        return  false;
     }
 
     /**
